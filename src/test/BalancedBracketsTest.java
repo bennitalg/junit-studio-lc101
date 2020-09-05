96f1b1b2becb85537fdec17bbc32d91954a1a19c
bennitalg <40967401+bennitalg@users.noreply.github.com>
2020-09-05 15:04:50 -0500

    Unit Testing Balanced Brackets Complete

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,72 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void testNoBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
 
+    @Test
+    public void testBracketsInTheFront() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Launchcode"));
+    }
+
+    @Test
+    public void testSplitBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void testBracketThatWord() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void testOneFrontBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void testOppositeFacingBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void testALonelyBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void testBackwardsBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testOneBracketFacingEast() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void testMultipleWorkingBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][][][][]"));
+    }
+
+    @Test
+    public void testWackyBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][][]["));
+    }
 }
